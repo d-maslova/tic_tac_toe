ef0043c111f2f54876ba4fab9cb72b1a80bc4362
Dasha M <you@example.com>
2022-05-24 15:22:08 +0200

    add break and turn_counter

--- main.py
+++ main.py
@@ -22,30 +22,35 @@ field = "_1_|_2_|_3_\n_4_|_5_|_6_\n_7_|_8_|_9_\n"
 start = input("Would you like to play tic tac toe? type y/n\n").lower()
 p1 = []
 p2 = []
+turn_counter = 0
 
 if start == "y":
     game_on = True
     matrix = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     print("Welcome to tic tac toe. Player 1 will start.\n")
     print(field)
-    while game_on:
+    while game_on and turn_counter <= 8:
         p1_turn = int(input(f"Player 1, choose a number between 1 and 9\n"))
         matrix[p1_turn - 1] = "X"
         p1.append(p1_turn)
         place_mark(matrix)
-
+        turn_counter += 1
         if check_result(p1):
             print("Player 1 wins")
+            print("Game Over.")
             game_on = False
+            break
 
         p2_turn = int(input(f"Player 2, choose a number between 1 and 9\n"))
         matrix[p2_turn - 1] = "0"
         p2.append(p2_turn)
         place_mark(matrix)
-
+        turn_counter +=1
         if check_result(p2):
             print("Player 2 wins")
             game_on = False
+
     else:
+        game_on = False
         print("Game over")
 
